07447761cdd9fe458905f74590f517e8cc72db5e
Adam Priest <adampriest@pop-os.localdomain>
2020-10-17 23:48:52 -0400

    added thumbnails, background color, font colors, resized pictures, etc.

---
+++ db/migrate/20201016004408_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.string :user_name
+      t.text :body
+      t.integer :rating
+      t.integer :post_id
+
+      t.timestamps
+    end
+  end
+end
